c90843d095144db413ee8bff5377b68e6f5d7388
river <riverdao36@gmail.com>
2026-06-02 15:29:42 +1000

    build: fix basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+  if (repoName) {
+    repo = `/${repoName}`;
+  }
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: repo,
   images: {
     unoptimized: true,
   },
